1b6149215528c07f36090e40e1fbd0d3c32244bc
Thierry RABEARIJAO <thierryrabearijao@gmail.com>
2017-09-18 21:59:46 +0200

    #master project base

---
+++ src/main/java/fr/raptao/App.java
@@ -0,0 +1,13 @@
+package fr.raptao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
