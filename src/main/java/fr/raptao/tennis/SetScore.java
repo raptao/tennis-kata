d9e50ee82fc7af6769baf9665acb3dd0e9fa768c
Thierry RABEARIJAO <thierryrabearijao@gmail.com>
2017-09-20 19:48:19 +0200

    #sprint2 set max score

---
+++ src/main/java/fr/raptao/tennis/SetScore.java
@@ -0,0 +1,25 @@
+package fr.raptao.tennis;
+
+/**
+ * Created by raptao on 9/20/2017.
+ */
+public class SetScore implements Score{
+
+    private final static int MAX_SCORE = 7;
+
+    private int score;
+
+    @Override
+    public boolean increment() {
+        if( score >= MAX_SCORE){
+            return false;
+        }
+        score ++;
+        return true;
+    }
+
+    @Override
+    public int currentScore() {
+        return score;
+    }
+}
